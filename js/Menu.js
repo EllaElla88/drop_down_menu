ad71e0a5f0a30aadbc6bbc72169f7c16bac21e5f
EllaElla88 <65270830+EllaElla88@users.noreply.github.com>
2021-08-30 17:40:39 +0800

    drop down menu
    手機電腦下拉式選單

---
+++ js/Menu.js
@@ -0,0 +1,25 @@
+﻿$(function() {
+
+    var BTN = $("#BLACK h6");
+    var MENU = $("#MENU");
+    var DOCUMENT = $(document);
+
+    BTN.on("touchstart click", AAA);
+
+	
+    function AAA(e) {
+        MENU.show();
+        MENU.on("touchstart click", BBB);
+        var WW = DOCUMENT.innerWidth();
+        var HH = DOCUMENT.innerHeight();
+        MENU.css({ width: WW, minHeight: HH });
+        e.stopImmediatePropagation();
+        e.preventDefault();
+    }
+
+    
+    $(window).on("resize", function() {
+        MENU.attr("style", "");
+    });
+
+});
---
+++ js/dropDownMenu.js
@@ -0,0 +1,44 @@
+$("#menu1").hover(
+    function() {
+        $("#slide1").stop(true, true).fadeIn(500);
+    },
+    function() {
+        $("#slide1").stop(true, true).fadeOut(500);
+    }
+);
+
+$("#menu2").hover(
+    function() {
+        $("#slide2").stop(true, true).slideDown(500);
+    },
+    function() {
+        $("#slide2").stop(true, true).slideUp(500);
+    }
+);
+
+$("#menu3").hover(
+    function() {
+        $("#slide3").stop(true, true).slideDown(500);
+    },
+    function() {
+        $("#slide3").stop(true, true).slideUp(500);
+    }
+);
+
+$("#menu4").hover(
+    function() {
+        $("#slide4").stop(true, true).slideDown(500);
+    },
+    function() {
+        $("#slide4").stop(true, true).slideUp(500);
+    }
+);
+
+$("#menu5").hover(
+    function() {
+        $("#slide5").stop(true, true).slideDown(500);
+    },
+    function() {
+        $("#slide5").stop(true, true).slideUp(500);
+    }
+);
